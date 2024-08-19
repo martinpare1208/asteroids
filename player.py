76b366a174782ca8fd01e524f98dc9bfef991401
martinpare1208 <martinapilado@gmail.com>
2024-08-19 18:31:42 -0400

    created player movement

--- player.py
+++ player.py
@@ -33,5 +33,17 @@ class Player(CircleShape):
         if keys[pygame.K_a]:
             dt = dt * -1
             self.rotate(dt)
+
+            
         if keys[pygame.K_d]:
             self.rotate(dt)
+        
+        if keys[pygame.K_w]:
+            self.move(dt)
+        
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt    
