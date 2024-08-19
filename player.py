5689aec449e781d299ac216a346f5d86a00d5eb3
martinpare1208 <martinapilado@gmail.com>
2024-08-19 17:45:38 -0400

    Implemented player rotation

--- player.py
+++ player.py
@@ -22,3 +22,16 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        
+    def rotate(self, dt):
+        self.rotation = dt * PLAYER_TURN_SPEED
+    
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            dt = dt * -1
+            self.rotate(dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
